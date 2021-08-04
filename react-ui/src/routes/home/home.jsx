d9e6cf95fb5cd0cf0f254b12a41b90d89f840c61
Ted Wetekamp <ted.wetekamp@yahoo.com>
2021-08-04 15:11:21 -0500

    Add article links to homepage

--- react-ui/src/routes/home/home.jsx
+++ react-ui/src/routes/home/home.jsx
@@ -15,6 +15,17 @@ const Home = () => {
           </div>
           <Greeting />
           <UsernameForm />
+          <div className="front-arts">
+            <h1>Helpful Articles for Better Software</h1>
+            <ul>
+              <li><h4><a href="https://dev.to/akashshyam/7-tips-for-clean-code-3nk1" target="_blank" rel="noreferrer" style={{color: "black"}}>7 Tips For Clean Code - DEV Community</a></h4></li>
+              <li><h4><a href="https://dev.to/ruppysuppy/5-ui-tips-to-become-a-better-front-end-developer-1ae1" target="_blank" rel="noreferrer" style={{color: "black"}}>5 UI Tips to Become a Better Front End Developer - DEV Community</a></h4></li>
+              <li><h4><a href="https://dev.to/nipu/tips-tricks-of-javascript-react-3ncc" target="_blank" rel="noreferrer" style={{color: "black"}}>TIPS and TRICKS OF JAVASCRIPT and REACT - DEV Community</a></h4></li>
+              <li><h4><a href="https://betterprogramming.pub/5-clean-code-tips-every-developer-should-know-by-heart-6b632e397278" rel="noreferrer" style={{color: "black"}}>5 Clean Code Tips Every Developer Should Know by Heart - Better Programming</a></h4></li>
+              <li><h4><a href="https://www.geeksforgeeks.org/12-tips-to-write-efficient-sql-queries/" target="_blank" rel="noreferrer" style={{color: "black"}}>12 Tips to Write Efficient SQL Queries - GeeksforGeeks</a></h4></li>
+              <li><h4><a href="https://dev.to/aidenybai/get-s-on-your-github-project-17o1" target="_blank" rel="noreferrer" style={{color: "black"}}>Tips to get Visibility + Feedback to your GitHub project - DEV Community</a></h4></li>
+            </ul>  
+          </div>  
         </>
     )
 };
